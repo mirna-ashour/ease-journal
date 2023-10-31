c307713d735704a526c9aa5d67168c8bde4f7a7a
Mahmoud1813 <mga8876@nyu.edu>
2023-10-31 02:19:50 -0400

    Added test for (GET) endpoint for users

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -9,13 +9,13 @@ def test_hello():
     print(f'{resp=}')
     resp_json = resp.get_json()
     print(f'{resp_json=}')
-    assert 'hello' in resp_json
+    assert ep.HELLO_RESP in resp_json
 
 
-#def test_list_users():
-    #resp = TEST_CLIENT.get(ep.USERS_EP)
-    #resp_json = resp.get_json()
-    #assert isinstance(resp_json, dict)
-    #assert ep.TITLE in resp_json
-    #assert ep.TYPE in resp_json
-    #assert ep.DATA in resp_json
+def test_list_users():
+    resp = TEST_CLIENT.get(ep.USERS_EP)
+    resp_json = resp.get_json()
+    assert isinstance(resp_json, dict)
+    assert ep.TITLE in resp_json
+    assert ep.TYPE in resp_json
+    assert ep.DATA in resp_json
