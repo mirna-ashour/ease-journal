c4363f91412f005595de12cd01fe89cad963f964
Elizabeth Vazquez <ev2070@nyu.edu>
2023-11-04 16:56:58 -0400

    Add a new journal member 'prompt' and update test_journals

--- data/journals.py
+++ data/journals.py
@@ -3,20 +3,26 @@ This module interfaces to our journal data.
 """
 
 TITLE = 'title'
-DEFAULT_TITLE = 'Untitled'
+PROMPT = 'prompt'
 CONTENT = 'content'
+
+DEFAULT_TITLE = 'Untitled'
+TEST_PROMPT = 'Reflect on a act of kindness.'
 
 journals = {
     "2023-10-27 08:30:00": {
         TITLE: "Morning Reflection",
+        PROMPT: TEST_PROMPT,
         CONTENT: "My alarm is broken...",
     },
     "2023-10-27 12:45:00": {
         TITLE: "Lunchtime Thoughts",
+        PROMPT: "Write about an aspiration.",
         CONTENT: "I was hungry, so...",
     },
     "2023-10-27 18:15:00": {
         TITLE: "Evening Musings",
+        PROMPT: "Did you step out of your comfort zone today?",
         CONTENT: "The moon looks like cheese...",
     },
 }
@@ -26,8 +32,16 @@ def get_journals() -> dict:
     return journals
 
 
-def add_journal(timestamp: str, title: str, content: str):
-    if title == "":
+def add_journal(timestamp: str, title: str, prompt: str, content: str):
+    # if title is empty, set title to default title
+    if not title:
         title = DEFAULT_TITLE
-    journal_entry = {TITLE: title, CONTENT: content}
+
+    # if prompt is duplicated, raise Value Error
+    prompt_dup = any(journal.get(PROMPT) ==
+                     prompt for journal in journals.values())
+    if prompt_dup:
+        raise ValueError(f'Duplicate journal entry prompt: {prompt=}')
+
+    journal_entry = {TITLE: title, PROMPT: prompt, CONTENT: content}
     journals[timestamp] = journal_entry
--- data/tests/test_journals.py
+++ data/tests/test_journals.py
@@ -1,35 +1,52 @@
-from datetime import datetime
+import pytest
 import data.journals as jrnls
+
+from datetime import datetime
+
 
 FORMAT = "%Y-%m-%d %H:%M:%S"
 
 """
     Ensure:
         - get_journals() returns a dict with at least 1 journal
-        - each journal key is a valid timestamp str
-        - each journal is a dict with title & content members
-        - each journal title is a str
-        - each journal entry content is a str
+        - each journal key is a valid timestamp (str)
+        - each journal is a dict with the following members:
+            - TITLE (str)
+            - PROMPT (str)
+            - CONTENT (str)
 """
 def test_get_journals():
     journals = jrnls.get_journals()
     assert isinstance(journals, dict)
     assert len(journals) > 0
+
     for key in journals:
         assert isinstance(key, str)
         assert isinstance(datetime.strptime(key, FORMAT), datetime)
+        
         journal = journals[key]
         assert isinstance(journal, dict)
+        
         assert jrnls.TITLE in journal
+        assert jrnls.PROMPT in journal
         assert jrnls.CONTENT in journal
+        
         j_title = journal[jrnls.TITLE]
         assert isinstance(j_title, str)
+
+        j_prompt = journal[jrnls.PROMPT]
+        assert isinstance(j_prompt, str)
+        
         j_content = journal[jrnls.CONTENT]
         assert isinstance(j_content, str)
 
 
 ADD_TIMESTAMP = '2000-01-01 09:57:00'
 ADD_TITLE = 'SOME TITLE'
+ADD_PROMPT0 = 'some prompt0'
+ADD_PROMPT1 = 'some prompt1'
+ADD_PROMPT2 = 'some prompt2'
+ADD_PROMPT3 = 'some prompt3'
 ADD_CONTENT = 'blah blah blah'
 
 
@@ -39,29 +56,49 @@ ADD_CONTENT = 'blah blah blah'
           it is in the list of journals
 """
 def test_add_journal():
-    jrnls.add_journal(ADD_TIMESTAMP, ADD_TITLE, ADD_CONTENT)
+    jrnls.add_journal(ADD_TIMESTAMP, ADD_TITLE, ADD_PROMPT0, ADD_CONTENT)
     journals = jrnls.get_journals()
     assert ADD_TIMESTAMP in journals
-    assert journals[ADD_TIMESTAMP] == {jrnls.TITLE: ADD_TITLE, jrnls.CONTENT: ADD_CONTENT}
+    assert journals[ADD_TIMESTAMP] == {
+         jrnls.TITLE: ADD_TITLE,
+         jrnls.PROMPT: ADD_PROMPT0,
+         jrnls.CONTENT: ADD_CONTENT
+        }
 
 
 def test_add_journal_without_title_or_content():
-    jrnls.add_journal(ADD_TIMESTAMP, "", "")
+    jrnls.add_journal(ADD_TIMESTAMP, "", ADD_PROMPT1, "")
     journals = jrnls.get_journals()
     assert ADD_TIMESTAMP in journals
-    assert journals[ADD_TIMESTAMP] == {jrnls.TITLE: jrnls.DEFAULT_TITLE, jrnls.CONTENT: ""}
+    assert journals[ADD_TIMESTAMP] == {
+         jrnls.TITLE: jrnls.DEFAULT_TITLE,
+         jrnls.PROMPT: ADD_PROMPT1,
+         jrnls.CONTENT: ""
+        }
 
 
 def test_add_journal_without_content():
-    jrnls.add_journal(ADD_TIMESTAMP, ADD_TITLE, "")
+    jrnls.add_journal(ADD_TIMESTAMP, ADD_TITLE, ADD_PROMPT2, "")
     journals = jrnls.get_journals()
     assert ADD_TIMESTAMP in journals
-    assert journals[ADD_TIMESTAMP] == {jrnls.TITLE: ADD_TITLE, jrnls.CONTENT: ""}
+    assert journals[ADD_TIMESTAMP] == {
+         jrnls.TITLE: ADD_TITLE,
+         jrnls.PROMPT: ADD_PROMPT2,
+         jrnls.CONTENT: ""
+        }
 
 
 def test_add_journal_without_title():
-    jrnls.add_journal(ADD_TIMESTAMP, "", ADD_CONTENT)
+    jrnls.add_journal(ADD_TIMESTAMP, "", ADD_PROMPT3, ADD_CONTENT)
     journals = jrnls.get_journals()
     assert ADD_TIMESTAMP in journals
-    assert journals[ADD_TIMESTAMP] == {jrnls.TITLE: jrnls.DEFAULT_TITLE, jrnls.CONTENT: ADD_CONTENT}
+    assert journals[ADD_TIMESTAMP] == {
+         jrnls.TITLE: jrnls.DEFAULT_TITLE,
+         jrnls.PROMPT: ADD_PROMPT3,
+         jrnls.CONTENT: ADD_CONTENT
+        }
 
+
+def test_add_journal_dup_prompt():
+    with pytest.raises(ValueError):
+        jrnls.add_journal(ADD_TIMESTAMP, "", jrnls.TEST_PROMPT, "")
