e4acfeeb80c7ebfb4c5637014b58db29ca429f27
Mahmoud1813 <mga8876@nyu.edu>
2023-12-06 20:57:55 -0500

    NOT A COMMIT, tried fixing common.sh

--- data/bkup.sh
+++ data/bkup.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
- # Script to backup production database to JSON files.
+# Script to backup production database to JSON files.
 
- . ./common.sh
+. ./common.sh
 
- for collection in ${JournalCollections[@]}; do
-     echo "Backing up $collection"
-     $EXP --collection=$collection --db=$DB --out=$BKUP_DIR/$collection.json $CONNECT_STR --username $USER --password $MONGODB_PASSWORD
- done
+for collection in ${JournalCollections[@]}; do
+    echo "Backing up $collection"
+    $EXP --collection=$collection --db=$DB --out=$BKUP_DIR/$collection.json $CONNECT_STR --username $USER --password $MONGODB_PASSWORD
+done
 
- git add $BKUP_DIR/*.json
- git add $BKUP_DIR/*.json
- git add $BKUP_DIR/*.json
- git commit $BKUP_DIR/*.json -m "Mongo DB backup"
- git pull origin master
- git push origin master
+git add $BKUP_DIR/*.json
+git add $BKUP_DIR/*.json
+git add $BKUP_DIR/*.json
+git commit $BKUP_DIR/*.json -m "Mongo DB backup"
+git pull origin master
+git push origin master
--- data/restore.sh
+++ data/restore.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
- # Script to backup production database to JSON files.
+# Script to backup production database to JSON files.
 
- . ./common.sh
+. ./common.sh
 
- for collection in ${JournalCollections[@]}; do
-     echo "Restoring $collection"
-     $IMP --db=$DB --collection $collection --drop --file $BKUP_DIR/$collection.json
- done
+for collection in ${JournalCollections[@]}; do
+    echo "Restoring $collection"
+    $IMP --db=$DB --collection $collection --drop --file $BKUP_DIR/$collection.json
+done
