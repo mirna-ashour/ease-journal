ad773d88908874450e885056ee69d259c77edf47
Mahmoud1813 <mga8876@nyu.edu>
2023-12-06 20:54:52 -0500

    NOT A COMMIT, tried fixing common.sh

--- data/common.sh
+++ data/common.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
- # Some common shell stuff.
+# Some common shell stuff.
 
- echo "Importing from common.sh"
+echo "Importing from common.sh"
 
- DB=ease-journal
- USER=mirnaashour
- CONNECT_STR="mongodb+srv://koukoumongo1.yud9b.mongodb.net/"
- if [ -z $DATA_DIR ]
- then
-     DATA_DIR=/home/runner/work/ease-journal/ease-journal/data
- fi
- BKUP_DIR=$DATA_DIR/bkup
- EXP=/usr/local/bin/mongoexport
- IMP=/usr/local/bin/mongoimport
+DB=ease-journal
+USER=mirnaashour
+CONNECT_STR="mongodb+srv://koukoumongo1.yud9b.mongodb.net/"
+if [ -z $DATA_DIR ]
+then
+    DATA_DIR=/home/runner/work/ease-journal/ease-journal/data
+fi
+BKUP_DIR=$DATA_DIR/bkup
+EXP=/usr/local/bin/mongoexport
+IMP=/usr/local/bin/mongoimport
 
- if [ -z $MONGODB_PASSWORD ]
- then
-     echo "You must set MONGODB_PASSWORD in your env before running this script."
-     exit 1
- fi
+if [ -z $MONGODB_PASSWORD ]
+then
+    echo "You must set MONGODB_PASSWORD in your env before running this script."
+    exit 1
+fi
 
 
- declare -a JournalCollections=("categories" "journals" "users")
+declare -a JournalCollections=("categories" "journals" "users")
